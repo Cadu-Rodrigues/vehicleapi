dc98af6188e3349f9d37f5228eaec4da65da871d
Carlos Eduardo <carloseduardordesousa@gmail.com>
2021-05-19 20:52:35 -0300

    fix: update the models

---
+++ src/main/java/com/zup/vehicleapi/model/User.java
@@ -0,0 +1,10 @@
+package com.zup.vehicleapi.model;
+
+import java.time.LocalDate;
+
+public class User {
+    private String name;
+    private String email;
+    private String CPF;
+    private LocalDate birthDate;
+}
---
+++ src/main/java/com/zup/vehicleapi/model/Vehicle.java
@@ -0,0 +1,9 @@
+package com.zup.vehicleapi.model;
+
+public class Vehicle {
+    public String brand;
+    private String model;
+    private int year;
+    private long value;
+    private int ownerCPF;
+}
