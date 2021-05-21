3201670261cc4cec8faacee559b1062d117d3c42
Carlos Eduardo <carloseduardordesousa@gmail.com>
2021-05-21 18:05:46 -0300

    config: add database setup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,24 @@
+INSERT INTO Users
+    (Name, Email, CPF, Birthdate)
+VALUES
+    ('Carlos', 'CarlosEmail@email.com', 1234, '1988-05-05');
+INSERT INTO Users
+    (Name, Email, CPF, Birthdate)
+VALUES
+    ('Amadeu', 'AmadeuEmail@email.com', 4321, '1989-05-05');
+INSERT INTO Users
+    (Name, Email, CPF, Birthdate)
+VALUES
+    ('Robson', 'RobsonEmail@email.com', 3333, '1964-05-05');
+INSERT INTO Vehicles
+    (Brand,Model,Year,Value, Ownerid)
+VALUES
+    ('Ford', 'Fiesta', 2009, 10000, 1);
+INSERT INTO Vehicles
+    (Brand,Model,Year,Value, Ownerid)
+VALUES
+    ('Ford', 'Fusion', 2009, 20000, 2);
+INSERT INTO Vehicles
+    (Brand,Model,Year,Value, Ownerid)
+VALUES
+    ('Ford', 'Ka', 2009, 30000, 1);
