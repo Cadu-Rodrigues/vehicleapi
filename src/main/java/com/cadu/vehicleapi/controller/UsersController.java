edcd6296f209de3dd30706ab2309a458a4a49bf0
Carlos Eduardo <carloseduardordesousa@gmail.com>
2021-05-21 18:08:27 -0300

    lint: remove unused packages

--- src/main/java/com/cadu/vehicleapi/controller/UsersController.java
+++ src/main/java/com/cadu/vehicleapi/controller/UsersController.java
@@ -1,12 +1,10 @@
 package com.cadu.vehicleapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 import com.cadu.vehicleapi.controller.DTO.UserDTO;
